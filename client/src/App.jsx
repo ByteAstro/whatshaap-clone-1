2a710cccf69ba8302ae5b0443f3c13bdb2a4affe
ByteAstro <biswajitswain348@gmail.com>
2024-04-24 06:14:00 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+// components ------------------
+
+
+import './App.css'
+import Messenger from './components/Messenger'
+
+function App() {
+
+  return (
+    <div className='bg-blue-200 h-screen w-full text-black'>
+      <Messenger />
+    </div>
+  )
+}
+
+export default App
---
+++ client/src/components/Messenger.jsx
@@ -0,0 +1,13 @@
+import LoginPage from "./authPages.jsx/LoginPage"
+
+const Messenger = () => {
+    return (
+        <div className="h-screen bg-[#DCDCDC]">
+            <div className="navbar h-52 bg-[#00BFA5]">
+            </div>
+            <LoginPage />
+        </div>
+    )
+}
+
+export default Messenger
